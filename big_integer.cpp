aa9f710dd56021b0e0d1fbd7f4e6173e176e87c7
golikovnik <golikovnik@gmail.com>
2019-05-18 11:55:20 +0300

    add hw3

--- big_integer.cpp
+++ big_integer.cpp
@@ -1,345 +1,370 @@
-#include "big_integer.h"
+//  Copyright 2019 Nikita Golikov
+
+#include "./big_integer.h"
+
+#include <utility>
+#include <algorithm>
 
 template <typename T>
 inline digit_t to_digit_t(T x) {
-    return static_cast<digit_t>(x);
+  return static_cast<digit_t>(x);
 }
 
 template <typename T>
 inline overflow_t to_overflow_t(T x) {
-    return static_cast<overflow_t>(x);
+  return static_cast<overflow_t>(x);
 }
 
-big_integer::big_integer(digit_t a) : sign(0), data{a} {
-    strip();
+big_integer::big_integer(digit_t a) : sign(0), data(a) {
+  strip();
 }
 
-big_integer::big_integer(int a) : sign(to_digit_t(-(a < 0))), data{to_digit_t(a)} {
-    strip();
+big_integer::big_integer(int a) : sign(to_digit_t(-(a < 0))),
+                                  data(to_digit_t(a)) {
+  strip();
 }
 
-big_integer::big_integer() : big_integer(0) {}
+big_integer::big_integer() : big_integer(0) {
+}
 
 big_integer::big_integer(std::string const& str) : big_integer() {
-    if (!str.empty()) {
-        int result_sign = 1;
-        size_t pos = 0;
-        if (str[0] == '-') {
-            result_sign = -1;
-            pos++;
-        } else if (str[0] == '+') {
-            pos++;
-        }
-        for (; pos < str.size(); pos++) {
-            if (str[pos] < '0' || str[pos] > '9') {
-                throw std::invalid_argument("invalid bigint representation");
-            }
-            (*this *= 10) += (str[pos] - '0') * result_sign;
-        }
+  if (!str.empty()) {
+    int result_sign = 1;
+    size_t pos = 0;
+    if (str[0] == '-') {
+      result_sign = -1;
+      pos++;
+    } else if (str[0] == '+') {
+      pos++;
     }
+    for (; pos < str.size(); pos++) {
+      if (str[pos] < '0' || str[pos] > '9') {
+        throw std::invalid_argument("invalid bigint representation");
+      }
+      (*this *= 10) += (str[pos] - '0') * result_sign;
+    }
+  }
 }
 
 big_integer& big_integer::add_sub(big_integer const& rhs, bool add) {
-    assure_size(std::max(size(), rhs.size()) + 2);
-    bool carry = false;
-    for (size_t i = 0; i < size(); i++) {
-        overflow_t temp = data[i];
-        if (add) {
-            (temp += rhs.digit_at(i)) += carry;
-        } else {
-            (temp -= rhs.digit_at(i)) -= carry;
-        }
-        data[i] = to_digit_t(temp);
-        carry = temp > MAX_DIGIT;
+  assure_size(std::max(size(), rhs.size()));
+  bool carry = false;
+  for (size_t i = 0; i < size(); i++) {
+    overflow_t temp = data[i];
+    if (add) {
+      (temp += rhs.digit_at(i)) += carry;
+    } else {
+      (temp -= rhs.digit_at(i)) -= carry;
     }
-    sign = data.back();
-    return strip();
+    data[i] = to_digit_t(temp);
+    carry = temp > MAX_DIGIT;
+  }
+  overflow_t one_more_ot = digit_at(size());
+  if (add) {
+    (one_more_ot += rhs.digit_at(size())) += carry;
+  } else {
+    (one_more_ot -= rhs.digit_at(size())) -= carry;
+  }
+  digit_t one_more = to_digit_t(one_more_ot);
+  carry = one_more_ot > MAX_DIGIT;
+  sign = add ? digit_at(size() + 1) + rhs.digit_at(size() + 1) + carry :
+          digit_at(size() + 1) - rhs.digit_at(size() + 1) - carry;
+  if (one_more != sign) {
+    data.push_back(one_more);
+  }
+  return strip();
 }
 
 big_integer& big_integer::operator+=(big_integer const& rhs) {
-    return add_sub(rhs, true);
+  return add_sub(rhs, true);
 }
 
 big_integer& big_integer::operator-=(big_integer const& rhs) {
-    return add_sub(rhs, false);
+  return add_sub(rhs, false);
 }
 
 big_integer& big_integer::strip() {
-    while (!data.empty() && data.back() == sign) {
-        data.pop_back();
-    }
-    return *this;
+  while (!data.empty() && data.back() == sign) {
+    data.pop_back();
+  }
+  return *this;
 }
 
 digit_t big_integer::digit_at(size_t i) const {
-    return i < size() ? data[i] : sign;
+  return i < size() ? data[i] : sign;
 }
 
 void big_integer::assure_size(size_t sz) {
-    data.resize(std::max(size(), sz), sign);
+  data.resize(std::max(size(), sz), sign);
 }
 
 bool operator==(big_integer const& a, big_integer const& b) {
-    return a.sign == b.sign && a.data == b.data;
+  return a.sign == b.sign && a.data == b.data;
 }
 
 big_integer big_integer::operator-() const {
-    big_integer temp = ~*this;
-    return temp += 1;
+  big_integer temp = ~*this;
+  return temp += 1;
 }
 
 big_integer big_integer::operator+() const {
-    return *this;
+  return *this;
 }
 
 big_integer big_integer::operator~() const {
-    big_integer copy = *this;
-    copy.sign ^= MAX_DIGIT;
-    for (digit_t& digit : copy.data) {
-        digit ^= MAX_DIGIT;
-    }
-    return copy;
+  big_integer copy = *this;
+  copy.sign ^= MAX_DIGIT;
+  for (size_t i = 0; i < size(); i++) {
+    copy.data[i] ^= MAX_DIGIT;
+  }
+  return copy;
 }
 
 big_integer& big_integer::operator++() {
-    return *this += 1;
+  return *this += 1;
 }
 
 big_integer operator+(big_integer a, big_integer const& b) {
-    return a += b;
+  return a += b;
 }
 
 big_integer operator-(big_integer a, big_integer const& b) {
-    return a -= b;
+  return a -= b;
 }
 
 big_integer operator&(big_integer a, big_integer const& b) {
-    return a &= b;
+  return a &= b;
 }
 
 big_integer operator|(big_integer a, big_integer const& b) {
-    return a |= b;
+  return a |= b;
 }
 
 big_integer operator^(big_integer a, big_integer const& b) {
-    return a ^= b;
+  return a ^= b;
 }
 
 big_integer& big_integer::operator*=(big_integer const& rhs) {
-    return *this = *this * rhs;
+  return *this = *this * rhs;
 }
 
 big_integer& big_integer::apply_bitwise_op(big_integer const& rhs,
-                                           std::function<digit_t(digit_t, digit_t)> const& op) {
-    assure_size(rhs.size());
-    for (size_t i = 0; i < size(); i++) {
-        data[i] = op(data[i], rhs.digit_at(i));
-    }
-    sign = op(sign, rhs.sign);
-    return strip();
+                                           std::function<digit_t(digit_t,
+                                                                 digit_t)> const& op) {
+  assure_size(rhs.size());
+  for (size_t i = 0; i < size(); i++) {
+    data[i] = op(data[i], rhs.digit_at(i));
+  }
+  sign = op(sign, rhs.sign);
+  return strip();
 }
 
 size_t big_integer::size() const {
-    return data.size();
+  return data.size();
 }
 
 big_integer& big_integer::operator&=(big_integer const& rhs) {
-    return apply_bitwise_op(rhs, std::bit_and<>());
+  return apply_bitwise_op(rhs, std::bit_and<>());
 }
 
 big_integer& big_integer::operator|=(big_integer const& rhs) {
-    return apply_bitwise_op(rhs, std::bit_or<>());
+  return apply_bitwise_op(rhs, std::bit_or<>());
 }
 
 big_integer& big_integer::operator^=(big_integer const& rhs) {
-    return apply_bitwise_op(rhs, std::bit_xor<>());
+  return apply_bitwise_op(rhs, std::bit_xor<>());
 }
 
 bool operator!=(big_integer const& a, big_integer const& b) {
-    return !(a == b);
+  return !(a == b);
 }
 
 big_integer big_integer::operator++(int) {
-    big_integer result = *this;
-    ++*this;
-    return result;
+  big_integer result = *this;
+  ++*this;
+  return result;
 }
 
 big_integer& big_integer::operator--() {
-    return *this -= 1;
+  return *this -= 1;
 }
 
 big_integer big_integer::operator--(int) {
-    big_integer result = *this;
-    --*this;
-    return result;
+  big_integer result = *this;
+  --*this;
+  return result;
 }
 
 bool operator<(big_integer const& a, big_integer const& b) {
-    if (a.sign != b.sign) {
-        return a.is_negative();
+  if (a.sign != b.sign) {
+    return a.is_negative();
+  }
+  if (a.size() != b.size()) {
+    return (a.size() < b.size()) ^ a.is_negative();
+  }
+  for (size_t i = a.size(); i-- > 0;) {
+    if (a.data[i] != b.data[i]) {
+      return a.data[i] < b.data[i];
     }
-    if (a.size() != b.size()) {
-        return (a.size() < b.size()) ^ a.is_negative();
-    }
-    return std::lexicographical_compare(
-            a.data.rbegin(), a.data.rend(), b.data.rbegin(), b.data.rend()
-    );
+  }
+  return false;
 }
 
 bool big_integer::is_negative() const {
-    return sign != 0;
+  return sign != 0;
 }
 
 bool operator>(big_integer const& a, big_integer const& b) {
-    return b < a;
+  return b < a;
 }
 
 bool operator<=(big_integer const& a, big_integer const& b) {
-    return !(a > b);
+  return !(a > b);
 }
 
 bool operator>=(big_integer const& a, big_integer const& b) {
-    return !(a < b);
+  return !(a < b);
 }
 
 big_integer big_integer::abs() const {
-    return is_negative() ? -*this : *this;
+  return is_negative() ? -*this : *this;
 }
 
 //  both numbers are non-negative
 big_integer multiply_by_digit(big_integer const& a, digit_t b) {
-    big_integer result;
-    result.assure_size(a.size() + 1);
-    digit_t carry = 0;
-    for (size_t i = 0; i < a.size(); i++) {
-        overflow_t product = to_overflow_t(a.data[i]) * b + carry;
-        result.data[i] = to_digit_t(product);
-        carry = to_digit_t(product >> DIGITS);
-    }
-    result.data.back() = carry;
-    return result.strip();
+  big_integer result;
+  result.assure_size(a.size() + 1);
+  digit_t carry = 0;
+  for (size_t i = 0; i < a.size(); i++) {
+    overflow_t product = to_overflow_t(a.data[i]) * b + carry;
+    result.data[i] = to_digit_t(product);
+    carry = to_digit_t(product >> DIGITS);
+  }
+  result.data.back() = carry;
+  return result.strip();
 }
 
 big_integer operator*(big_integer a, big_integer b) {
-    digit_t result_sign = a.sign ^b.sign;
-    a = a.abs();
-    b = b.abs();
-    if (b.size() > a.size()) {
-        a.data.swap(b.data);
-    }
-    big_integer result;
-    for (digit_t digit : b.data) {
-        result += multiply_by_digit(a, digit);
-        a.data.insert(a.data.begin(), 0);
-    }
-    return result.set_sign(result_sign);
+  digit_t result_sign = a.sign ^b.sign;
+  a = a.abs();
+  b = b.abs();
+  if (b.size() > a.size()) {
+    a.data.swap(b.data);
+  }
+  big_integer result;
+  for (size_t i = 0; i < b.size(); i++) {
+    result += multiply_by_digit(a, b.data[i]);
+    a.data.push_front(0);
+  }
+  return result.set_sign(result_sign);
 }
 
 big_integer& big_integer::operator/=(big_integer const& rhs) {
-    return *this = *this / rhs;
+  return *this = *this / rhs;
 }
 
 big_integer& big_integer::operator%=(big_integer const& rhs) {
-    return *this = *this % rhs;
+  return *this = *this % rhs;
 }
 
 big_integer operator/(big_integer const& a, big_integer const& b) {
-    return divmod(a, b).first;
+  return divmod(a, b).first;
 }
 
 big_integer operator%(big_integer const& a, big_integer const& b) {
-    return divmod(a, b).second;
+  return divmod(a, b).second;
 }
 
 std::pair<big_integer, big_integer> divmod(big_integer a, big_integer b) {
-    if (b == 0) {
-        throw std::invalid_argument("division by zero");
+  if (b == 0) {
+    throw std::invalid_argument("division by zero");
+  }
+  digit_t q_sign = a.sign ^b.sign;
+  digit_t r_sign = a.sign;
+  a = a.abs();
+  b = b.abs();
+  int shift = 0;
+  for (overflow_t b_first = b.data.back(); b_first < BASE / 2;
+       shift++, b_first *= 2) {
+  }
+  a <<= shift;
+  b <<= shift;
+  big_integer q, r;
+  q.assure_size(a.size());
+  for (size_t i = a.size(); i-- > 0;) {
+    r.data.push_front(0);
+    r += a.data[i];
+    overflow_t divi = (to_overflow_t(r.digit_at(b.data.size())) << DIGITS) |
+                      r.digit_at(b.data.size() - 1);
+    digit_t quo = to_digit_t(divi / b.data.back());
+    r -= multiply_by_digit(b, quo);
+    while (r.is_negative()) {
+      r += b;
+      quo--;
     }
-    digit_t q_sign = a.sign ^b.sign;
-    digit_t r_sign = a.sign;
-    a = a.abs();
-    b = b.abs();
-    int shift = 0;
-    for (overflow_t b_first = b.data.back(); b_first < BASE / 2; shift++, b_first *= 2);
-    a <<= shift;
-    b <<= shift;
-    big_integer q, r;
-    q.assure_size(a.size());
-    for (size_t i = a.size(); i-- > 0;) {
-        r.data.insert(r.data.begin(), 0);
-        r += a.data[i];
-        overflow_t divi = (to_overflow_t(r.digit_at(b.data.size())) << DIGITS) | r.digit_at(b.data.size() - 1);
-        digit_t quo = to_digit_t(divi / b.data.back());
-        r -= multiply_by_digit(b, quo);
-        while (r.is_negative()) {
-            r += b;
-            quo--;
-        }
-        q.data[i] = quo;
-    }
-    r >>= shift;
-    return {q.set_sign(q_sign).strip(), r.set_sign(r_sign)};
+    q.data[i] = quo;
+  }
+  r >>= shift;
+  return {q.set_sign(q_sign).strip(), r.set_sign(r_sign)};
 }
 
 big_integer& big_integer::set_sign(digit_t new_sign) {
-    return sign == new_sign ? strip() : *this = -*this;
+  return sign == new_sign ? strip() : *this = -*this;
 }
 
 big_integer& big_integer::operator<<=(int rhs) {
-    if (rhs < 0) {
-        return *this >>= (-rhs);
+  if (rhs < 0) {
+    return *this >>= (-rhs);
+  }
+  data.push_front(0, rhs / DIGITS);
+  data.push_back(sign);
+  digit_t carry = 0;
+  unsigned left = rhs % DIGITS;
+  if (left != 0) {
+    for (size_t i = 0; i < size(); i++) {
+      std::tie(data[i], carry) = std::make_pair((data[i] << left) | carry,
+                                                data[i] >> (DIGITS - left));
     }
-    data.insert(data.begin(), rhs / DIGITS, 0);
-    data.push_back(sign);
-    digit_t carry = 0;
-    unsigned left = rhs % DIGITS;
-    if (left != 0) {
-        for (size_t i = 0; i < size(); i++) {
-            std::tie(data[i], carry) = std::make_pair(
-                    (data[i] << left) | carry, data[i] >> (DIGITS - left)
-            );
-        }
-    }
-    return strip();
+  }
+  return strip();
 }
 
 big_integer operator<<(big_integer a, int b) {
-    return a <<= b;
+  return a <<= b;
 }
 
 big_integer& big_integer::operator>>=(int rhs) {
-    if (rhs < 0) {
-        return *this <<= (-rhs);
+  if (rhs < 0) {
+    return *this <<= (-rhs);
+  }
+  data.pop_front(std::min<size_t>(data.size(), rhs / DIGITS));
+  digit_t carry = sign;
+  unsigned left = rhs % DIGITS;
+  if (left != 0) {
+    for (size_t i = size(); i-- > 0;) {
+      std::tie(data[i], carry) = std::make_pair(
+              (carry << (DIGITS - left)) | (data[i] >> left),
+              data[i] & ((1u << left) - 1));
     }
-    data.erase(data.begin(), data.begin() + std::min<size_t>(data.size(), rhs / DIGITS));
-    digit_t carry = sign;
-    unsigned left = rhs % DIGITS;
-    if (left != 0) {
-        for (size_t i = size(); i-- > 0;) {
-            std::tie(data[i], carry) = std::make_pair(
-                    (carry << (DIGITS - left)) | (data[i] >> left), data[i] & ((1u << left) - 1)
-            );
-        }
-    }
-    return strip();
+  }
+  return strip();
 }
 
 big_integer operator>>(big_integer a, int b) {
-    return a >>= b;
+  return a >>= b;
 }
 
 std::string to_string(big_integer a) {
-    bool negative = a.is_negative();
-    a = a.abs();
-    std::string result;
-    big_integer r;
-    do {
-        std::tie(a, r) = divmod(a, 10);
-        result += static_cast<char>('0' + r.digit_at(0));
-    } while (a != 0);
-    if (negative) {
-        result += '-';
-    }
-    return {result.rbegin(), result.rend()};
+  bool negative = a.is_negative();
+  a = a.abs();
+  std::string result;
+  big_integer r;
+  do {
+    std::tie(a, r) = divmod(a, 10);
+    result += static_cast<char>('0' + r.digit_at(0));
+  } while (a != 0);
+  if (negative) {
+    result += '-';
+  }
+  return {result.rbegin(), result.rend()};
 }
--- big_integer.h
+++ big_integer.h
@@ -1,10 +1,14 @@
-#ifndef CPP_HOMEWORKS_BIG_INTEGER_H
-#define CPP_HOMEWORKS_BIG_INTEGER_H
+//  Copyright 2019 Nikita Golikov
+
+#ifndef BIG_INTEGER_H_
+#define BIG_INTEGER_H_
 
 #include <string>
-#include <vector>
 #include <functional>
 #include <limits>
+#include <utility>
+
+#include "./my_vector.cpp"
 
 using digit_t = uint32_t;
 using overflow_t = uint64_t;
@@ -14,115 +18,120 @@ unsigned const DIGITS = std::numeric_limits<digit_t>::digits;
 overflow_t const BASE = static_cast<overflow_t>(MAX_DIGIT) + 1;
 
 struct big_integer {
-    big_integer();
+  big_integer();
 
-    big_integer(big_integer const& other) = default;
+  big_integer(big_integer const& other) = default;
 
-    big_integer(digit_t a);
+  big_integer(digit_t a);
 
-    big_integer(int a);
+  big_integer(int a);
 
-    explicit big_integer(std::string const& str);
+  explicit big_integer(std::string const& str);
 
-    ~big_integer() = default;
+  ~big_integer() = default;
 
-    big_integer& operator=(big_integer const& other) = default;
+  big_integer& operator=(big_integer const& other) = default;
 
-    big_integer& operator+=(big_integer const& rhs);
+  big_integer& operator+=(big_integer const& rhs);
 
-    big_integer& operator-=(big_integer const& rhs);
+  big_integer& operator-=(big_integer const& rhs);
 
-    big_integer& add_sub(big_integer const& rhs, bool add);
+  big_integer& add_sub(big_integer const& rhs, bool add);
 
-    big_integer& operator*=(big_integer const& rhs);
+  big_integer& operator*=(big_integer const& rhs);
 
-    big_integer& operator/=(big_integer const& rhs);
+  big_integer& operator/=(big_integer const& rhs);
 
-    big_integer& operator%=(big_integer const& rhs);
+  big_integer& operator%=(big_integer const& rhs);
 
-    big_integer& apply_bitwise_op(big_integer const& rhs,
-                                  std::function<digit_t(digit_t, digit_t)> const& op);
+  big_integer& apply_bitwise_op(big_integer const& rhs,
+                                std::function<digit_t(digit_t,
+                                                      digit_t)> const& op);
 
-    big_integer& operator&=(big_integer const& rhs);
+  big_integer& operator&=(big_integer const& rhs);
 
-    big_integer& operator|=(big_integer const& rhs);
+  big_integer& operator|=(big_integer const& rhs);
 
-    big_integer& operator^=(big_integer const& rhs);
+  big_integer& operator^=(big_integer const& rhs);
 
-    big_integer& operator<<=(int rhs);
+  big_integer& operator<<=(int rhs);
 
-    big_integer& operator>>=(int rhs);
+  big_integer& operator>>=(int rhs);
 
-    big_integer operator+() const;
+  big_integer operator+() const;
 
-    big_integer operator-() const;
+  big_integer operator-() const;
 
-    big_integer abs() const;
+  big_integer abs() const;
 
-    big_integer operator~() const;
+  big_integer operator~() const;
 
-    big_integer& operator++();
+  big_integer& operator++();
 
-    big_integer operator++(int);
+  big_integer operator++(int);
 
-    big_integer& operator--();
+  big_integer& operator--();
 
-    big_integer operator--(int);
+  big_integer operator--(int);
 
-    friend bool operator==(big_integer const& a, big_integer const& b);
+  friend bool operator==(big_integer const& a, big_integer const& b);
 
-    friend bool operator!=(big_integer const& a, big_integer const& b);
+  friend bool operator!=(big_integer const& a, big_integer const& b);
 
-    friend bool operator<(big_integer const& a, big_integer const& b);
+  friend bool operator<(big_integer const& a, big_integer const& b);
 
-    friend bool operator>(big_integer const& a, big_integer const& b);
+  friend bool operator>(big_integer const& a, big_integer const& b);
 
-    friend bool operator<=(big_integer const& a, big_integer const& b);
+  friend bool operator<=(big_integer const& a, big_integer const& b);
 
-    friend bool operator>=(big_integer const& a, big_integer const& b);
+  friend bool operator>=(big_integer const& a, big_integer const& b);
 
-    friend big_integer operator+(big_integer a, big_integer const& b);
+  friend big_integer operator+(big_integer a, big_integer const& b);
 
-    friend big_integer operator-(big_integer a, big_integer const& b);
+  friend big_integer operator-(big_integer a, big_integer const& b);
 
-    friend big_integer operator*(big_integer a, big_integer b);
+  friend big_integer operator*(big_integer a, big_integer b);
 
-    friend std::pair<big_integer, big_integer> divmod(big_integer a, big_integer b);
+  friend std::pair<big_integer, big_integer>
+  divmod(big_integer a, big_integer b);
 
-    friend big_integer operator/(big_integer const& a, big_integer const& b);
+  friend big_integer operator/(big_integer const& a, big_integer const& b);
 
-    friend big_integer operator%(big_integer const& a, big_integer const& b);
+  friend big_integer operator%(big_integer const& a, big_integer const& b);
 
-    friend big_integer operator&(big_integer a, big_integer const& b);
+  friend big_integer operator&(big_integer a, big_integer const& b);
 
-    friend big_integer operator|(big_integer a, big_integer const& b);
+  friend big_integer operator|(big_integer a, big_integer const& b);
 
-    friend big_integer operator^(big_integer a, big_integer const& b);
+  friend big_integer operator^(big_integer a, big_integer const& b);
 
-    friend big_integer operator<<(big_integer a, int b);
+  friend big_integer operator<<(big_integer a, int b);
 
-    friend big_integer operator>>(big_integer a, int b);
+  friend big_integer operator>>(big_integer a, int b);
 
-    friend std::string to_string(big_integer a);
+  friend std::string to_string(big_integer a);
 
-private:
+  friend std::ostream& operator<<(std::ostream& out, big_integer const& a) {
+    return out << to_string(a);
+  }
 
-    digit_t sign;
-    std::vector<digit_t> data;
+ private:
+  digit_t sign;
+  my_vector<digit_t> data;
 
-    big_integer& strip();
+  big_integer& strip();
 
-    big_integer& set_sign(digit_t sign);
+  big_integer& set_sign(digit_t sign);
 
-    size_t size() const;
+  size_t size() const;
 
-    digit_t digit_at(size_t i) const;
+  digit_t digit_at(size_t i) const;
 
-    void assure_size(size_t sz);
+  void assure_size(size_t sz);
 
-    bool is_negative() const;
+  bool is_negative() const;
 
-    friend big_integer multiply_by_digit(big_integer const& a, digit_t b);
+  friend big_integer multiply_by_digit(big_integer const& a, digit_t b);
 };
 
-#endif // CPP_HOMEWORKS_BIG_INTEGER_H
+#endif  // BIG_INTEGER_H_
